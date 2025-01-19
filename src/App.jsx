869b803c059bb62297d835da3d32dfd3e5198276
BuildTools <unconfigured@null.spigotmc.org>
2025-01-20 00:12:53 +0100

    A - Game board visualisation and some functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Player from "./components/Player"
+import GameBoard from "./components/GameBoard"
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
           <Player initialName="Player1" symbol="X" />
           <Player initialName="Player2" symbol="O" />
         </ol>
+      <GameBoard></GameBoard>
       </div>
     </main>
   )
